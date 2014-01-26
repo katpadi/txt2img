280e9f4278e37b393f7ca03c4261af267171b2c8
Kat Padilla <katp@sakurasky.com>
2014-01-26 16:00:48 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Txt2img::Application.config.secret_key_base = 'b3694becc0ebf8b6254220686f12adf31d303c2da88ebeb66a31f5560663f558cbcf7875530a25a5f0e0a54389c41e32aecc888b7b5124acc4bc170e54f61651'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Txt2img::Application.config.session_store :cookie_store, key: '_txt2img_session'
